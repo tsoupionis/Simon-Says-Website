f6e6f7fcb411c6903e2a1a568b64ddbe3a6e3cf2
tsoupionis <thomassoup@yahoo.com>
2023-02-26 23:50:30 -0500

    Updated Simon Says Website 2/26

--- game.js
+++ game.js
@@ -1,17 +1,33 @@
 var buttonColors = ["red", "blue", "green", "yellow"]; //all colors in Simon Says
 var gamePattern = []; // initialize game pattern
+var userClickedPattern = []; // initialize user clicked pattern
 nextSequence();
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id"); // picks the button color you click on 
+    userClickedPattern.push(userChosenColor); // gets added to pattern
+
+    playSound(userChosenColor); // plays the sound when clicked 
+    
+    animatePress(userChosenColor);
+})
 
 function nextSequence () {
     var randomNumber = Math.floor(Math.random() * 4); // random number between 0 and 3
     var randomChosenColor = buttonColors[randomNumber]; // assigns a random color using the random number
     gamePattern.push(randomChosenColor); //keeps the game going by adding each color into a pattern 
-    
-     
-    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    
-    $("#" + randomChosenColor).click(function () {
-        var audio = new Audio("sounds/" + randomChosenColor + ".mp3")
-        audio.play();
-    });
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100) // flash animation 
+    playSound(randomChosenColor); // plays the sound when of color chosen for pattern
+}
+
+function playSound (name) {
+    var audio = new Audio("sounds/" + name + ".mp3")
+    audio.play();
+}
+
+function animatePress (currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed"); }, 100);
 }
