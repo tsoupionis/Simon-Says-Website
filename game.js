7ecd3d732b0ba7c5db0b1d74b1427ce4ced8deb8
tsoupionis <thomassoup@yahoo.com>
2023-02-26 22:00:25 -0500

    Updated Simon Says Website 2/26

--- game.js
+++ game.js
@@ -1,9 +1,17 @@
 var buttonColors = ["red", "blue", "green", "yellow"]; //all colors in Simon Says
 var gamePattern = []; // initialize game pattern
-
+nextSequence();
 
 function nextSequence () {
     var randomNumber = Math.floor(Math.random() * 4); // random number between 0 and 3
     var randomChosenColor = buttonColors[randomNumber]; // assigns a random color using the random number
     gamePattern.push(randomChosenColor); //keeps the game going by adding each color into a pattern 
+    
+     
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    $("#" + randomChosenColor).click(function () {
+        var audio = new Audio("sounds/" + randomChosenColor + ".mp3")
+        audio.play();
+    });
 }
